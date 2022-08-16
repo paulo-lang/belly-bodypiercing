03a667650768d0631ec4f39e715645b07e46ab5b
paulo-lang <paulo.santos02321@gmail.com>
2022-08-15 21:42:40 -0300

    feat(header): new router

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,13 @@
 import React, { useState } from 'react';
-import { HashRouter, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
-import Home from './Components/Pages/Home/index.js'
+import Navbar from './Components/Navbar/index.js';
 
 export default function Routes() {
-    const [toggleMenu, setToggleMenu] = useState(false);
-
     return (
-        <HashRouter>
-            <div className='header'>
-                
-            </div>
-            <div>
-                <div className="header">
-                    
-                </div>
-                <div>
-                    <Route path="/" exact component={Home} />
-                </div>
-            </div>
-        </HashRouter>
+        <BrowserRouter>
+            <Navbar/>
+        </BrowserRouter>
     );
 }
