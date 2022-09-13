bd3e9ee457cde9b51dd7b5fd4ffbfb12c0dfc9f9
Paulo J. Santos <71194592+paulo-lang@users.noreply.github.com>
2022-09-13 13:16:39 -0300

    fix(lottie): removed lottie

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -3,7 +3,6 @@ import 'react-sticky-header/styles.css';
 import { HomeContainer, BgImg, BgWeb, ArrowContainer, BgImgContainer } from './HomeElements';
 import BackImg from '../../Images/backImage.jpg'
 import BackWeb from '../../Images/bgweb.png'
-import Lottie from 'react-lottie';
 import animationData from '../../assets/arrow-down.json';
 
 const Home = () => {
@@ -23,11 +22,6 @@ const Home = () => {
                 <BgImg src={BackImg}/>
                 <BgWeb src={BackWeb}/>
                 <ArrowContainer>
-                    <Lottie options={defaultOptions}
-                        height={300}
-                        width={300}
-                        isStopped={false}
-                        isPaused={false}/>
                 </ArrowContainer>
                 
             </BgImgContainer>
